f62fae01ae4bc9148e07dd98e9a5c8adf6c13b82
adhlssu07 <adhlssu07@gmail.com>
2011-06-03 20:44:19 -0700

    Adding Configuration Link

--- lib/spree_flexi_rate_shipping_hooks.rb
+++ lib/spree_flexi_rate_shipping_hooks.rb
@@ -1,3 +1,5 @@
 class SpreeFlexiRateShippingHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
+   insert_after :admin_configurations_menu do
+     "<%= configurations_menu_item('Flexible Rate Shipping', admin_flexi_shipping_rates_path, 'Flexible Rate Shipping') %>"
+   end
 end
