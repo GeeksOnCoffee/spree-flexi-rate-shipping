09d40f4c2a090d9a0dd539a13e40b59589f9aa7c
adhlssu07 <adhlssu07@gmail.com>
2011-06-04 16:49:30 -0700

    Removed code that was causing error, not sure it's necessary.

--- app/controllers/admin/flexi_shipping_rates_controller.rb
+++ app/controllers/admin/flexi_shipping_rates_controller.rb
@@ -3,9 +3,6 @@ class Admin::FlexiShippingRatesController < Admin::ResourceController
     layout 'admin'
 
     private 
-			def collection 
-	      @collection ||= end_of_association_chain.order_by_zone_and_category 
-	    end
 	
 	    def load_data     
 	      @available_categories = ShippingCategory.find :all, :order => :name
